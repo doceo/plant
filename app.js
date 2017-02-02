2d3d3ece50184cfdf78a7b87857477a5f24b0056
diome <diomede.mazzone@gmail.com>
2017-02-02 08:26:00 +0100

    configurazione della comunicazione socket

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
-var express = require('express');
+var express = require('express').createServer();
+var app = express();
+
 var db= require('./gestione');
-var io = require('socket.io')(server);
+
+var io = require('socket.io')(app);
+
 var server =require('http').Server(app);
  
-var app = express();
 
 app.get('/dato/tutti', function (req, res) {
 	res.send(db.gestioneDati.tutti());
@@ -15,7 +18,7 @@ app.get('/', function(req, res){
 
 });
 
-app.post('/dato/acquisisci/:dato', function (req, res) {
+app.get('/dato/acquisisci/:dato', function (req, res) {
 	db.gestioneDati.nuovo(req.params.dato);
 	res.end();
 });
@@ -24,13 +27,16 @@ app.post('/dato/acquisisci/:dato', function (req, res) {
 io.on('connection',function(socket){
 
   // all'avvio di una connessione viene creato un socket!
-
-  console.log('user connected');
+	socket.emit('news', {hello; 'world' });
+	socket.on('my other event', function (data)
+ 	console.log('ricevuto:');
+	console.log(data);
+ 
 
 
 });
 
-app.listen(4156, function () {
+app.listen(8080, function () {
   console.log('Per inviare dati tramite metodo Post: /dato/acquisisci/:dato');
   console.log('Per visualizzare l\'elenco di tutti i dati acquisiti tramite metodo Get: /dato/tutti');
   console.log('...');
