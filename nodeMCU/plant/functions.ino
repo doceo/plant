ec161f91cb374fc41b8e5c0f0f3bf0dd832c8627
diome <diomede.mazzone@gmail.com>
2017-02-17 21:20:46 +0100

    invio dati random a tempo

---
+++ nodeMCU/plant/functions.ino
@@ -0,0 +1,38 @@
+
+void connectWifi()
+{
+  Serial.print("Connecting to "+*MY_SSID);
+  WiFi.begin(MY_SSID, MY_PWD);
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("Connected");
+  Serial.println("");  
+}//end connect
+ 
+void sendRnd(int rnd)
+{  
+   WiFiClient client;
+ 
+   if (client.connect(server, 3000)) { // use ip 184.106.153.149 or api.thingspeak.com
+   Serial.println("WiFi Client connected ");
+   
+   String postStr= String(rnd);
+
+   String url = "/dato/acquisisci/" + postStr;
+   Serial.println(url);
+
+   client.print(url);
+//   client.print("\n\n");
+//   client.print(postStr);
+ 
+   
+   delay(1000);
+   
+   }//end if
+   sent++;
+ client.stop();
+}//end send
--- nodeMCU/plant/plant.ino
+++ nodeMCU/plant/plant.ino
@@ -1,11 +1,11 @@
 
 #include <ESP8266WiFi.h>
 
+const char* server = "192.168.1.67";
+const char* MY_SSID = "____";
+const char* MY_PWD = "___";
 
-const char* server = "192.168.1.18";
 
-const char* MY_SSID = "_____";
-const char* MY_PWD = "_____";
 int temp = 1000;
 int sent = 0;
 void setup() {
@@ -19,48 +19,10 @@ void loop() {
   rnd = random(1,100);
 
 //  Serial.print(String(sent)+" Temperature: ");
-//  Serial.println(temp);
+  Serial.println(rnd);
   
   sendRnd(rnd);
 
   delay(temp);
 }
  
-void connectWifi()
-{
-  Serial.print("Connecting to "+*MY_SSID);
-  WiFi.begin(MY_SSID, MY_PWD);
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print(".");
-  }
- 
-  Serial.println("");
-  Serial.println("Connected");
-  Serial.println("");  
-}//end connect
- 
-void sendRnd(int rnd)
-{  
-   WiFiClient client;
- 
-   if (client.connect(server, 4156)) { // use ip 184.106.153.149 or api.thingspeak.com
-   Serial.println("WiFi Client connected ");
-   
-   String postStr= String(rnd);
-
-//   postStr += "\r\n\r\n";
-   
-//   client.print("POST /update HTTP/1.1\n");
-//   client.print("Connection: close\n");
-//   client.print("Content-Type: application/x-www-form-urlencoded\n");
-//   client.print("Content-Length: ");
-   client.print('dato/acquisisci/');
-//   client.print("\n\n");
-   client.print(postStr);
-   delay(1000);
-   
-   }//end if
-   sent++;
- client.stop();
-}//end send
