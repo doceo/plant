82d59fad369fc4c01757984424ebd2c0cea21e95
Doceo <diomede.mazzone@gmail.com>
2017-02-23 12:04:32 +0100

    inserimento dei commenti

--- nodeMCU/plant/functions.ino
+++ nodeMCU/plant/functions.ino
@@ -16,18 +16,25 @@ void connectWifi()
 void sendRnd(int rnd)
 {  
    WiFiClient client;
- 
+  
+   //cerca di stabilire una connessione al server sulla porta 3000, 
+   //nel caso ci dovesse riuscire invia al monitor seriale un feedback
    if (client.connect(server, 3000)) { 
    Serial.println("WiFi Client connected ");
    
+   //converte in oggetto Stringa il parametro ricevuto
    String postStr= String(rnd);
 
+   //costruisce la stringa da inviare, si noti il parametro GET che denota il metodo scelto
    String url = "GET /dato/acquisisci/" + postStr + " HTTP/1.1";
    Serial.println(postStr);
 
+   //tutte le istruzioni sono duplicate per poter inviare la stessa sequenza di dati 
+   //al server Node e al monitor seriale
    client.println(url);
    Serial.println(url);
    
+   //per poter inviare correttamente una richiesta di tipo GET bisogna indicare l'HOST di destniazione
    client.println("Host: 192.168.1.67:3000");
    Serial.println("Host: 192.168.1.67:3000");
    
@@ -35,7 +42,8 @@ void sendRnd(int rnd)
    Serial.println("Connection: close");
      
    client.println();
-     
+   
+   //generiamo il beep di notifica dell'invio del dato.
    Serial.println();
    digitalWrite(14, HIGH);            
    delay(500);
