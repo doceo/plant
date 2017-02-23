5602beb2b436e7817fb5b92500f11bb49c56dfaa
Doceo <diomede.mazzone@gmail.com>
2017-02-23 11:51:44 +0100

    aggiunta di commenti

--- nodeMCU/plant/plant.ino
+++ nodeMCU/plant/plant.ino
@@ -1,15 +1,19 @@
 
 #include <ESP8266WiFi.h>
 
+//definisco le costanti che determinano la connessione. Bisogna sostituire l'indirizzo IP della macchina a cui collegarsi.
 const char* server = "192.168.1.67";
 const char* MY_SSID = "____";
 const char* MY_PWD = "___";
 
 
 int temp = 1000;
-int sent = 0;
+
+
 void setup() {
   Serial.begin(115200);
+
+  //richiamo la funzione che gestisce la connessione
   connectWifi();
 }
  
@@ -18,7 +22,6 @@ void loop() {
 
   rnd = random(0,40);
 
-//  Serial.print(String(sent)+" Temperature: ");
   Serial.println(rnd);
   
   sendRnd(rnd);
