e7a30c0e52aa27a230d356088ddcdb006c252c1f
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-02-25 15:42:44 +0100

    aggiunto db

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const MongoClient = require('mongodb').MongoClient;
 
-//inizializiamo le variabili relative ai moduli utili all anostra applicazione.
+var db;
 
-//il framework
-var app = require('express')();
-
-//il server
-var server = require('http').Server(app);
-
-//la gestione della comunicazione via socket
-var io = require('socket.io')(server);
-
-//la gestione delle informazioni. in questo caso è quasi fittizio, perchè non sono salvate in modo permanente.
-var db = require('./gestione');
+MongoClient.connect('mongodb://172.17.0.3:27017/terreno', (err, database) => {
+  if (err) return console.log(err)
+  db = database;
+  server.listen(3000, () => {
+    console.log('Server in ascolto sulla porta 3000 ...');
+  });
+})
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname+'/html/index.html');
 });
 
+
 // abbiamo usato una GET ma sarebbe più opportuno usare il metodo POST. la scelta del GET
-// perchè risulta più comodo nel caso non si disponga di dispositivi fisici e si voglia 
+// perchè risulta più comodo nel caso non si disponga di dispositivi fisici e si voglia
 // testare l'applicativo da browser
-app.get('/dato/acquisisci/:dato', function (req, res) {
-  var temperature = req.params.dato;
-  db.gestioneDati.nuovo(temperature);
-  console.log('acquisisco il dato');
+app.get('/acquisisci/:dato', function (req, res) {
   res.end();
 
-  io.emit('newTemperature', { t: new Date(), temperature: temperature });
+  var temperature = {
+    t: new Date(),
+    temperature: req.params.dato,
+  };
+  db.collection('temperatures').insert(temperature);
+
+  io.emit('newTemperature', temperature);
 });
 
 io.on('connection', function (socket) {
   console.log('richiesta di connessione dal client');
   console.log('invio tutte le temperature');
-  io.emit('temperatures', db.gestioneDati.tutti());
-});
-
-server.listen(3000, function () {
-  console.log('Per inviare dati tramite metodo GET: /dato/acquisisci/:dato');
-  //console.log('Per visualizzare l\'elenco di tutti i dati acquisiti tramite metodo GET: /dato/tutti');
-  console.log('...');
-  console.log('Server in esecuzione sulla porta 3000...');
+  db.collection('temperatures').find().toArray( (err, result) => {
+    io.emit('temperatures', result);
+  });
 });
