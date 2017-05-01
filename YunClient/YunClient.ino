d05c1ee4abad37173afbaf3a15bc02dc7a1597e4
Diomede Mazzone <diomede.mazzone@gmail.com>
2017-05-01 21:53:48 +0200

    aggiunto come client arduino yun

---
+++ YunClient/YunClient.ino
@@ -0,0 +1,29 @@
+
+#include <Process.h>
+
+String server = "http://192.168.1.92:3000";
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int terra;
+
+String url;
+
+
+void setup() {
+  // Initialize Bridge
+  Bridge.begin();
+
+  // Initialize Serial
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // Do nothing here.
+  runCurl();
+  Serial.print("leggo...");
+  terra = analogRead(A0);
+  Serial.print(terra);
+  delay(300000);
+}
+
---
+++ YunClient/function.ino
@@ -0,0 +1,19 @@
+
+void runCurl() {
+  // Launch "curl" command and get Arduino ascii art logo from the network
+ 
+  terra = analogRead(sensorPin);
+  url = server + "/acquisisci/" + String(terra);
+  
+  // curl is command line program for transferring data using different internet protocols
+  Process p;    // Create a process and call it "p"
+  p.begin("curl");  // Process that launch the "curl" command
+  p.addParameter("GET"); // Add the URL parameter to "curl"
+  p.addParameter(url); // Add the URL parameter to "curl"
+  p.run();    // Run the process and wait for its termination
+  Serial.print("dato inviato");
+  // Print arduino logo over the Serial
+  // A process output can be read with the stream methods
+  // Ensure the last bit of data is sent.
+  Serial.flush();
+}
