75ca9e2614c149bb6c724bc4452b0fad0a762d92
Diomede Mazzone <diomede.mazzone@gmail.com>
2018-04-20 08:30:02 +0200

    riorganizzazione dei file e delle cartelle

--- Arduino/mqtt_simulatore_eth/mqtt_simulatore_eth.ino
+++ Arduino/mqtt_simulatore_eth/mqtt_simulatore_eth.ino
@@ -32,7 +32,7 @@ int value = 0;
 // Update these with values suitable for your network.
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 230);
-IPAddress server(192, 168, 1, 69);
+IPAddress server(192, 168, 1, 174);
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -56,14 +56,14 @@ void reconnect() {
       Serial.println("connected");
 
       // ... and resubscribe
-      client.subscribe("acqDati");
+//      client.subscribe("acqDati");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
-    }
+      }
   }
 }
 
@@ -72,7 +72,7 @@ void setup()
   Serial.begin(57600);
 
   client.setServer(server, 1883);
-  client.setCallback(callback);
+//  client.setCallback(callback);
 
   Ethernet.begin(mac, ip);
   // Allow the hardware to sort itself out
@@ -84,7 +84,7 @@ void loop()
   if (!client.connected()) {
     reconnect();
   }
-  client.loop();
+//  client.loop();
 
   
   
