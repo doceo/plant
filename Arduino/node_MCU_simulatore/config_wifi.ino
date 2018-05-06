1e75bf5cb3cc1aca7825d241c4b27efbd6fbee2d
Doceo <diomede.mazzone@gmail.com>
2018-05-06 20:17:57 +0200

    Delete config_wifi.ino

--- Arduino/node_MCU_simulatore/config_wifi.ino
+++
@@ -1,6 +0,0 @@
-
-//definisco le costanti che determinano la connessione. Bisogna sostituire l'indirizzo IP della macchina a cui collegarsi.
-
-const char* MY_SSID = "makappa";
-const char* MY_PWD = "liberdiom";
-
