47c8b67635e574a2a98815287bbc8f29b644b54c
Diomede Mazzone <diomede.mazzone@gmail.com>
2018-05-06 20:04:44 +0200

    aggiunta si un simulatore wifi

---
+++ Arduino/node_MCU_simulatore/config_wifi.ino
@@ -0,0 +1,6 @@
+
+//definisco le costanti che determinano la connessione. Bisogna sostituire l'indirizzo IP della macchina a cui collegarsi.
+
+const char* MY_SSID = "makappa";
+const char* MY_PWD = "liberdiom";
+
