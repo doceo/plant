f72e217d67d370c7dc4e8a0e47f25741f47758d1
Diomede Mazzone <diomede.mazzone@gmail.com>
2018-05-28 18:00:51 +0200

    collegamento DB heroku

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(log('dev'));
 //connessione del client a mongodb
-MongoClient.connect('mongodb://127.0.0.1:27017/terreno', function (err, database) {
+MongoClient.connect('mongodb://diomDB:Neru65da_@ds237620.mlab.com:37620/heroku_zn28dl17', function (err, database) {
 
   if (err) return console.log(err)
   db = database;
