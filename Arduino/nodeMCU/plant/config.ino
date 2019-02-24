6322d85eadff13b52325155ab9cd2588473b7a4b
Doceo <diomede.mazzone@gmail.com>
2019-02-24 22:53:50 +0100

    Update config.ino

--- Arduino/nodeMCU/plant/config.ino
+++ Arduino/nodeMCU/plant/config.ino
@@ -2,8 +2,8 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME    "diomede"
-#define IO_KEY         "30e0f9943a9243768e5c6af82fbbc16a"
+#define IO_USERNAME    "XXXX"
+#define IO_KEY         "XXXX"
 
 #define MQTT_SERVER "io.adafruit.com"
 #define MQTT_PORT 1883
